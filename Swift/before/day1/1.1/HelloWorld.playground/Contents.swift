d680808a6ad947f67d2daeeb2e6a2a9125ce9e90
増渕佳輝 <yoshxj4@gmail.com>
2018-03-26 08:08:30 +0900

    add

--- Swift/before/day1/1.1/HelloWorld.playground/Contents.swift
+++ Swift/before/day1/1.1/HelloWorld.playground/Contents.swift
@@ -6,5 +6,25 @@ let o: String? = "o"
 var world: String! = nil
 world = " world!"
 
-//let helloWorld: String
-//print(helloWorld)
+let helloWorld: String
+
+if let h2 = h , let o2 = o, let world2 = world {
+    helloWorld = h2 + ell + o2 + world2
+} else {
+    helloWorld = ""
+}
+
+//check flat map methods
+let helloWorld2: String = [h, ell, o, world].flatMap { $0 }.joined()
+
+let helloWorld3: String = [h, ell, o, world].reduce(""){ result, value in
+    guard let value = value else {
+        return ""
+    }
+    return result + value
+}
+
+
+print(helloWorld2)
+print(helloWorld)
+
